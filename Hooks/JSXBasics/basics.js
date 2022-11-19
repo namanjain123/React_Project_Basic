8bc611cbc64339804fddc5fd05d95ae7fab9cb03
namanjain123 <naman.nishi@gmail.com>
2022-11-19 13:27:59 +0530

    have added new and other jsx practice with more javascript samples

---
+++ Hooks/JSXBasics/basics.js
@@ -0,0 +1,9 @@
+const jsxElement = <h1>I am a JSX element</h1>
+const welcome = <h1>Welcome to 30 Days of React Challenge</h1>
+const data = <small>Oct 2, 2020</small>
+// JSX element
+const footer = (
+    <footer>
+      <p>Copyright 2020</p>
+    </footer>
+  )
---
+++ Hooks/JSXBasics/heeader_smaple.js
@@ -0,0 +1,10 @@
+const header = (
+    <header
+      style={{ border: '2px solid orange', color: 'black', fontSize: '18px' }}
+    >
+      <h2>Getting Started React</h2>
+      <h3>JavaScript Library</h3>
+      <p>Naman Jain</p>
+      <small>Oct 2, 2020</small>
+    </header>
+  )
