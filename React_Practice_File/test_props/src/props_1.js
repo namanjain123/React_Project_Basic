bd802214a16527592559d71617f9d8b89ba60803
namanjain123 <naman.nishi@gmail.com>
2022-11-22 22:57:04 +0530

    addition of props new file

---
+++ React_Practice_File/test_props/src/props_1.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Header Component
+const Header = (props) => {
+  console.log(props) // empty object, {}
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>{welcome}</h1>
+        <h2>{title}</h2>
+        <h3>{subtitle}</h3>
+        <p>
+          {author.firstName} {author.lastName}
+        </p>
+        <small>{date}</small>
+      </div>
+    </header>
+  )
+}
+
+// The App, or the parent or the container component
+// Functional Component
+const App = () => {
+  return (
+    <div className='app'>
+      <Header />
+    </div>
+  )
+}
+
+const rootElement = document.getElementById('root')
+
+ReactDOM.render(<App />, rootElement)
---
+++ React_Practice_File/test_props/src/props_string.js
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+// Header Component
+const Header = (props) => {
+  console.log(props)
+  return (
+    <header>
+      <div className='header-wrapper'>
+        <h1>{props.welcome}</h1>
+        <h2>{props.title}</h2>
+        <h3>{props.subtitle}</h3>
+        <p>
+          {props.firstName} {props.lastName}
+        </p>
+        <small>{props.date}</small>
+      </div>
+    </header>
+  )
+}
+
+// The App, or the parent or the container component
+// Functional Component
+const App = () => (
+  <div className='app'>
+    <Header
+      welcome='Welcome to 30 Days Of React'
+      title='Getting Started React'
+      subtitle='JavaScript Library'
+      firstName='Asabeneh'
+      lastName='Yetayeh'
+      date='Oct 4, 2020'
+    />
+  </div>
+)
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
