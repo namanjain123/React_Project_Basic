0c494de2bb845fc449477df4e71e7593ca74df77
namanjain123 <naman.nishi@gmail.com>
2022-11-25 22:18:04 +0530

    added new map the array react project and others

---
+++ React_Practice_File/test_mapping_array/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Array_test } from "./Array_Test";
+function App() {
+  return (
+    <div className="App">
+      
+        <Array_test/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ React_Practice_File/test_mapping_array/src/Array_Test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+const App = () => {
+  return (
+    <div className='container'>
+      <div>
+        <h1>Numbers List</h1>
+        {[1, 2, 3, 4, 5]}
+      </div>
+    </div>
+  )
+}
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
