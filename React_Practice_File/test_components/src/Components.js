3a8e5249e4068299fd1354b6692660e1211649f3
namanjain123 <naman.nishi@gmail.com>
2022-12-04 19:07:19 +0530

    new file added for higher components

---
+++ React_Practice_File/test_components/src/Components.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Button = ({ onClick, text, style }) => {
+  return (
+    <button onClick={onClick} style={style}>
+      {text}
+    </button>
+  )
+}
+
+const buttonWithStyle = (CompParam) => {
+  const buttonStyles = {
+    backgroundColor: '#61dbfb',
+    padding: '10px 25px',
+    border: 'none',
+    borderRadius: 5,
+    margin: 3,
+    cursor: 'pointer',
+    fontSize: 18,
+    color: 'white',
+  }
+  return (props) => {
+    return <CompParam {...props} style={buttonStyles} />
+  }
+}
+const NewButton = buttonWithSuperPower(Button)
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Button text='No Style' />
+        <NewButton text='Styled Button' />
+      </div>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<App />, rootElement)
