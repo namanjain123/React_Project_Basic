aa1f7924cc40cf67f5f2f9905bd4469fc562ad44
namanjain123 <naman.nishi@gmail.com>
2022-12-05 22:04:30 +0530

    addition of router test

---
+++ React_Practice_File/test_react_router/src/BrowserRouter.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+class BrowseRouter extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <h1>React Router DOM</h1>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<BrowseRouter />, rootElement)
---
+++ React_Practice_File/test_react_router/src/Switchs.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Home component
+const Home = (props) => <h1>Welcome Home</h1>
+// About component
+const About = (props) => <h1>About Us</h1>
+// Contact component
+const Contact = (props) => <h1>Contact us</h1>
+// Challenge component
+const Challenges = (props) => (
+  <div>
+    <h1>30 Days Of React Challenge</h1>
+  </div>
+)
+
+class Switchs extends Component {
+  render() {
+    return (
+      <Router>
+        <div className='App'>
+          <Switch>
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/challenges' component={Challenges} />
+            <Route exact path='/' component={Home} />
+          </Switch>
+        </div>
+      </Router>
+    )
+  }
+}
+
+const rootElement = document.getElementById('root')
+ReactDOM.render(<Switchs />, rootElement)
